831282f700e850f5e9975d5d8267dcff85f688ac
jwc20 <choi.jae100@gmail.com>
2025-08-06 17:26:21 +0900

    add openapi info

--- bncapi/urls.py
+++ bncapi/urls.py
@@ -5,7 +5,17 @@ from ninja_extra import NinjaExtraAPI
 from knoxtokens.auth import TokenAuthentication
 from users.api import UserAPI
 
-api = NinjaExtraAPI(auth=TokenAuthentication())
+api = NinjaExtraAPI(
+    openapi_extra={
+        "info": {
+            "title": "bncapi",
+            "description": "this is the bncapi",
+            "public": True,
+            # "version": "1.0.0",
+        }
+    },
+    auth=TokenAuthentication(),
+)
 api.register_controllers(UserAPI)
 urlpatterns = [
     path("admin/", admin.site.urls),
